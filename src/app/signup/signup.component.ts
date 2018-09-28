324340a01e44d72bd73b63abfdbc037ce28aea8e
Alsha Samantaray <alshasamantaray@alshas-mbp.3500.dhcp.asu.edu>
2018-09-28 01:23:31 -0700

    Profile Field validations To Edit the User Profile data.

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -70,8 +70,7 @@ export class SignupComponent implements OnInit {
   ngOnInit() {
   }
   validateEmail(email) {
-    //var re = ;
-    //return re.test(String(email).toLowerCase());
-    return true;
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
   }
 }
