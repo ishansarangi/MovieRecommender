cffe7a578e2702da82fba867313f6c9dfd5f6e99
Ishan Kumar Sarangi <ishan.sipun1@gmail.com>
2018-10-03 23:12:31 -0700

    signin

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -1,16 +1,49 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpParams, HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Login } from './login';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
+  endpoint = 'http://192.168.99.100:8081/login';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/x-www-form-urlencoded',
+      
+    })
+  };
+  username:string;
+  password:string;
+
   constructor(private http: HttpClient) { }
 
-  // public getCustomerList(): Observable<Login> {
-  //   // return (this.http.post(url:"localhost:8080",body:) );
-  // }
-}
+  private extractData(res: Response) {
+    let body = res;
+    return body || { };
+  }
+
+  login(username: string, password: string): Observable<Login> {
+    const body = new HttpParams()
+    .set('username', username)
+    .set('password', password);
+    return this.http.post<Login>(this.endpoint, body.toString(), this.httpOptions);
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+  
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+  
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+  
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+} 
--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {LoginService} from '../login.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -6,13 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
+  username: string;
+  userPassword: string;
+  errorMessage: string;
 
-  constructor() { }
+
+  constructor(private api: LoginService, private router: Router) { }
 
   ngOnInit() {
   }
 
   performLogin(){
+    this.errorMessage = '';
+    if (this.username.length == 0) {
+      this.errorMessage = "Please enter a valid UserName!";
+      console.log("UserName error");
+    }
 
+    this.api.login(
+      this.username,
+      this.userPassword
+    )
+      .subscribe(
+        r => {
+          console.log(r)
+          if (r.success) {
+            console.log(r)
+            // this.customer.setToken(r.token);
+            // this.router.navigateByUrl('/dashboard');
+
+          }
+        },
+        r => {
+          console.log(r)
+          alert(r.error.error);
+        });
+      
   }
 }
